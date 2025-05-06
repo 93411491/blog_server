a86a096fa6a32f2f877a1ae79a7792ede6020394
wangzhirui <wangzhirui@stepfun.com>
2025-05-06 18:41:22 +0800

    user luo ji

--- blog1/src/router/user.js
+++ blog1/src/router/user.js
@@ -1,17 +1,17 @@
 const { isGet } = require("./util/utils");
+const { loginCheck } = require("../controller/user");
+const { SuccessModel, ErrorModel } = require("../model/resModel");
 
 const handleUserRouter = (req, res) => {
-  const method = req.method;
-  const url = req.url;
-  const path = url.split("?")[0];
-  const query = url.split("?")[1];
-  console.log(`method :${method} , url : ${url} , path : ${path} , query : ${query}`);
-
-
   if (isGet(method) && path === "/api/user/login") {
-    return {
-      msg: "这是登录接口",
-    };
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (loginCheck(username, password)) {
+      return new SuccessModel("登录成功");
+    } else {
+      return new ErrorModel("等于失败");
+    }
   }
 };
 
